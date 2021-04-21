dc22e091b6524159e46401ea5ab6ee134646c17f
Ashkan Pourkand <ashkan.pourkand@gmail.com>
2021-04-20 20:15:13 -0600

    Update simple826.hpp

--- simple826.hpp
+++ simple826.hpp
@@ -1,8 +1,9 @@
-#ifndef TEL826_H
-#define TEL826_H
+#ifndef SIMP826_H
+#define SIMP826_H
 /*****************************************************
 A wrapper for sensoray card (826) to handle the ms updating rate!
 Ver 1.0 by Ashkan Oct-2020		
+In case of confusion please use: https://www.sensoray.com/downloads/man_826_hw_3.0.10.pdf#page=12&zoom=100,108,382
 *****************************************************/
 #include <chrono>
 #include "826api.h"
@@ -19,21 +20,21 @@ class Simple826{
 		// Helpful macros for DIOs
 		uint board;                        // change this if you want to use other than board number 0
 		int errcode;  
-		int boardflags;        // open 826 driver and find all 826 boards
-
+		int boardflags;       			   // open 826 driver and find all 826 boards
+		#define TSETTLE    7        	   // Settling delay after switching AIN (adjust as necessary).
+		#define SLOTFLAGS  0xFFFF   	   // Timeslot flags: use all 16 timeslots. -10V to +10V 0x0000 to 0xFFFF
+		int adcbuf[16]; 				   // Pointer to a buffer that will receive ADC results for the sixteen possible slots. The buffer must be large enough to
+		uint slotlist = 0xFFFF; 		   //slot list for Adc_read
 	public:
-		Simple826();
-		~Simple826();
-		void SetDacOutput(uint *chan, double *volts);  //chan->channel number  volt->voltage
-		void GetDacOutput(uint *chan, double *volts);  //chan->channel number  volt->voltage !!!! THIS IS NOT An ANALOG READ (this just returns the output current value)----
-		int GetError();  // Retruns error 
+		Simple826();	// Currently it sets the range for all of the analog channels to -10:10V. Keep the os
+		~Simple826();	//Explicitly turns all of the pins off. If there are actuators atached to the card keep them
+		void SetDacOutput(uint *chan, double *volts);  //chan->channel number  volt->voltage										 ==============ANALOGOUT============
+		void GetDacOutput(uint *chan, double *volts);  //chan->channel number  volt->voltage !!!! THIS IS NOT A ANALOG READ (this just returns the output current value)----
+		void SetDioOutput(uint *chan, bool *val);	   //chan->channel number  val->voltage 										 ==============DIGITALOUT===========
+		void ReadAdcOutput(double *data); //Read analog input adcbuf->buffer value  data->voltage received from board   ==============ANALOGREAD===========
+		void ReadAdcOutput(int* adcbuf, double *data); //Read analog input adcbuf->buffer value  data->voltage received from board
+		int GetError();    // Retruns error 
 		void PrintError(); //Prints error
-		// cv::Mat GetCurrentD2A();	//Returns the current frame from the camera (Type : opencv Mat)
-		// cv::Mat GetCurrentA2D();	//Returns the current frame from the camera (Type : opencv Mat)
-		// cv::Mat FilterSetup();	//Returns the current frame from the camera (Type : opencv Mat)
-		// cv::Mat FilterSignal();
-		// cv::Point GetTargetPose();
-		void SaveCurrentFrame(std::string fileName); // Updates and saves the current frame to the filename. You can draw the coordiante for the marker and also add the markers ID for ALL OF THE IDs.   
-		void FilterCurrentFrame(); // This is a place holder for the filter process if case you want to implement something later   
+		void SetToZero(); // Setting all channels to zero
 };
-#endif // TEL826_H
+#endif // SIMP826_H
